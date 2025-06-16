4c35cc7d7ae8a95a583b7ec78fcc60bfd412c4e2
rajugit6256 <rajushk9525@gmail.com>
2025-06-17 04:16:33 +0530

    signup

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
   res.send('<h1>this is login page</h1>');
 });
+app.get('/signup', (req, res) => {
+  res.send('<h1>this is login page</h1>');
+});
 
 
 app.listen(port, () => {
