4d35450fc527402a09c612a6c2551da8474cc104
Rajushk <rajukumarcrrit@gmail.com>
2025-06-17 02:56:10 +0530

    first commit backend

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');  
+require('dotenv').config()
+const app= express();
+
+const port = process.env.PORT ;
+
+app.get('/', (req, res) => {
+  res.send('This is raju kumar');
+});
+
+app.get('/login', (req, res) => {
+  res.send('<h1>this is login page</h1>');
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
