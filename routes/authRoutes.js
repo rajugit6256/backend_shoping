ddb9e778f07e32547a20765c0269696ed8a26459
rajugit6256 <rajushk9525@gmail.com>
2025-11-29 10:10:00 +0530

    build issue fixed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const loginUser = require('../controller/loginUser');
-const createUser = require('../controller/Auth');
+const createUser = require('../controller/auth');
 const verifyOtp = require('../controller/verifyOtp');
 const reSendOtp = require('../controller/reSendOtp');
-const profile = require('../controller/profile');
+// const profile = require('../controller/profile');
 
 
 router.post("/login", loginUser);
 router.post("/signup", createUser);
 router.post("/verify-otp", verifyOtp);
 router.post("/resend-otp", reSendOtp);
-router.get("/profile", profile);
+// router.get("/profile", profile);
 
 module.exports = router;        
